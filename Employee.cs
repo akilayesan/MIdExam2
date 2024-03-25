ec0e68b002d087b0ec73737c7507d537f808be13
AkilaYesan <akilayesan@gmail.com>
2024-03-25 11:54:16 +0530

    Class with Method

---
+++ Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Employee
+{
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public double BasicSalary { get; set; }
+    static void Print(string FirstName, string LastName, double BasicSalary)
+    {
+        string fullname = FirstName + " " + LastName;
+        double salary = BasicSalary;
+        Console.WriteLine("Name: " + fullname + "," + "Salary: " + salary);
+    }
+   
+}
+
--- Program.cs
+++ Program.cs
@@ -4,8 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Akila";
+            employee1.LastName = "Yesan";
+            employee1.BasicSalary = 50000.00;
+
+            Print(employee1.FirstName,employee1.LastName,employee1.BasicSalary);
+
 
         }
+       
     }
 }
