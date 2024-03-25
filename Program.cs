e2754cc8850551d56c7f401692ea33b11aa429d4
AkilaYesan <akilayesan@gmail.com>
2024-03-25 09:55:26 +0530

    total and average

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace MIdExam2
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MIdExam2
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {  
+            int[] numbers = {1,2,3,4,5,6,7,8,9,10,};
+            double total=0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                total = total + numbers[i];
+            }           
+            double average = 0.00;
+            average = total / numbers.Length;
+            Console.WriteLine("Total of the array : " + total);
+            Console.WriteLine("Average of the array : " + average);
+
+
+
 
         }
     }
